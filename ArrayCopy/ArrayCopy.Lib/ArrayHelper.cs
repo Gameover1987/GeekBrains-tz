e86ea492cdd68c76cb34ad7d139f24a2656b641d
icapgameover <icap.gameover@gmail.com>
2022-06-25 15:29:20 +0700

    .git ignore Добавил проект с функционалом по копированию массива

---
+++ ArrayCopy/ArrayCopy.Lib/ArrayHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace ArrayCopy.Lib
+{
+    public static class ArrayHelper<T>
+    {
+        public static T[] MakeCopy(T[] sourceArray)
+        {
+            T[] resultArray = new T[sourceArray.LongLength];
+
+            for (int i = 0; i < sourceArray.LongLength; i++)
+            {
+                resultArray[i] = sourceArray[i];
+            }
+
+            return resultArray;
+        }
+    }
+}
